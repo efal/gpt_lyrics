66f85bbe8651ba385f0433163c44ced006929a27
efal <32618426+efal@users.noreply.github.com>
2025-11-17 22:15:05 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open('lyrics-pwa-v1').then(cache => {
+    return cache.addAll(['/','/index.html','/manifest.json']);
+  }));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
